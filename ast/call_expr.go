064c0fd8f66f6c8f8d1b8674a9be5b4e805b9414
Konstantin8105 <konstantin8105@gmail.com>
2018-07-10 23:34:20 +0300

    AST: Call_expr parse vals

--- ast/call_expr.go
+++ ast/call_expr.go
@@ -1,22 +1,65 @@
 package ast
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type Call_expr struct {
 	Type string
 	Fn   string
+	Vals []string
 }
 
 func parse_call_expr(line string) (n Node) {
-	groups := groupsFromRegex(
-		`
-	type:(?P<type>.*) +
-	fn  :(?P<fn>.*) +
-	`,
-		line,
-	)
-	return Call_expr{
-		Type: strings.TrimSpace(groups["type"]),
-		Fn:   strings.TrimSpace(groups["fn"]),
+
+	var c Call_expr
+	var index int
+
+	if index = strings.Index(line, "type:"); index > 0 {
+		begin := index + 6
+		for i := begin; i < len(line); i++ {
+			if line[i] == ' ' {
+				c.Type = line[begin:i]
+				break
+			}
+		}
 	}
+
+	if index = strings.Index(line, "fn  :"); index > 0 {
+		begin := index + 6
+		for i := begin; i < len(line); i++ {
+			if line[i] == ' ' {
+				c.Fn = line[begin:i]
+				break
+			}
+		}
+	}
+
+	for counter := 0; ; counter++ {
+		var name string
+		if counter < 10 {
+			name = fmt.Sprintf("%d   :", counter)
+		} else {
+			name = fmt.Sprintf("%d  :", counter)
+		}
+
+		if index = strings.Index(line, name); index > 0 {
+			begin := index + 6
+			for i := begin; i < len(line); i++ {
+				if line[i] == ' ' {
+					c.Vals = append(c.Vals, line[begin:i])
+					break
+				}
+				if i == len(line)-1 {
+					c.Vals = append(c.Vals, line[begin:])
+					break
+				}
+			}
+		} else {
+			break
+		}
+	}
+
+	return c
 }
