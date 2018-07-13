605a267e9153f931df661f4de8c75bdd19f63c57
Konstantin8105 <konstantin8105@gmail.com>
2018-07-13 03:28:32 +0300

    AST: ast/identifier_node.go

--- ast/identifier_node.go
+++ ast/identifier_node.go
@@ -1,5 +1,10 @@
 package ast
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Identifier_node struct {
 	Lngt string
 	Strg string
@@ -9,8 +14,18 @@ func (a Identifier_node) GenNodeName() string {
 	return "identifier_node"
 }
 func parse_identifier_node(line string) (n Node) {
-	return Identifier_node{
+	s := Identifier_node{
 		Lngt: findVal("lngt:", &line),
-		Strg: findVal("strg:", &line),
 	}
+
+	i, err := strconv.Atoi(s.Lngt)
+	index := strings.Index(line, "strg:")
+	if index < 0 || err != nil {
+		s.Strg = findVal("strg:", &line)
+		return s
+	}
+
+	s.Strg = line[index+6 : index+6+i]
+
+	return s
 }
