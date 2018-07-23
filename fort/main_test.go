63f8950e7a3325d25f66d211ae22288e479de6d5
Konstantin8105 <konstantin8105@gmail.com>
2018-07-23 04:06:57 +0300

    minimaze fort

--- fort/main_test.go
+++ fort/main_test.go
@@ -2,18 +2,21 @@ package fort
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
 func TestScanner(t *testing.T) {
-	// src is the input that we want to tokenize.
-	src := []byte("cos(x) + 1i*sin(x) // Euler")
+	dat, err := ioutil.ReadFile("position.go")
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	// Initialize the scanner.
 	var s Scanner
-	fset := NewFileSet()                            // positions are relative to fset
-	file := fset.AddFile("", fset.Base(), len(src)) // register input "file"
-	s.Init(file, src, nil /* no error handler */, ScanComments)
+	fset := NewFileSet()                                       // positions are relative to fset
+	file := fset.AddFile("position.go", fset.Base(), len(dat)) // register input "file"
+	s.Init(file, dat, nil /* no error handler */, ScanComments)
 
 	// Repeated calls to Scan yield the token sequence found in the input.
 	for {
@@ -22,32 +25,5 @@ func TestScanner(t *testing.T) {
 			break
 		}
 		fmt.Printf("%s\t%s\t%q\n", fset.Position(pos), tok, lit)
-	}
-}
-
-func TestParser(t *testing.T) {
-	fset := NewFileSet() // positions are relative to fset
-
-	src := `package foo
-
-import (
-	"fmt"
-	"time"
-)
-
-func bar() {
-	fmt.Println(time.Now())
-}`
-
-	// Parse src but stop after processing the imports.
-	f, err := ParseFile(fset, "", src, ImportsOnly)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Print the imports from the file's AST.
-	for _, s := range f.Imports {
-		fmt.Println(s.Path.Value)
 	}
 }
