9fd1cfc998ffc860c45fe8e3480afb9500cdf082
Konstantin8105 <konstantin8105@gmail.com>
2018-08-04 16:09:57 +0300

    add test

--- testdata/main.f
+++ testdata/main.f
@@ -2,8 +2,10 @@
             integer i
             i = 12
             i = i + 12
+            i = i - 20
+            i = i *2
             write (*, FMT = 120) i
             STOp
 *
-  120 Format (' output ', I2 , ' integer')
+  120 Format (' output ', I1 , ' integer')
         END
