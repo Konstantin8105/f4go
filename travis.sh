db74f1a7365476b64cd6f7c859f1a25022ee83ce
Konstantin8105 <konstantin8105@gmail.com>
2018-08-10 14:47:24 +0300

    add travis points

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ export PKGS=$(go list ./...)
 # Make comma-separated.
 export PKGS_DELIM=$(echo "$PKGS" | paste -sd "," -)
 
-go list -f 'go test -v -covermode atomic -coverprofile {{.Name}}.coverprofile -coverpkg $PKGS_DELIM {{.ImportPath}}' $PKGS | xargs -I{} bash -c "{}"
+go list -f 'go test -v -covermode atomic -coverprofile {{.Name}}.coverprofile -coverpkg $PKGS_DELIM {{.ImportPath}} ./...' $PKGS | xargs -I{} bash -c "{}"
 
 # Merge coverage profiles.
 COVERAGE_FILES=`ls -1 *.coverprofile 2>/dev/null | wc -l`
