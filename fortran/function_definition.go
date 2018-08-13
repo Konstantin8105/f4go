11d5b3edee440236a5440a32825c86ad3af83cdd
Konstantin8105 <konstantin8105@gmail.com>
2018-08-14 02:47:22 +0300

    start inst func

--- fortran/function_definition.go
+++ fortran/function_definition.go
@@ -2,21 +2,20 @@ package fortran
 
 import goast "go/ast"
 
-var intrinsicFunction = []struct {
-	functionName string
-	typeNames    []string
-	changeFunc   func(*goast.FuncDecl)
-}{
-	{
-		functionName: "real",
-		typeNames:    []string{"float64"},
+type ifunc struct {
+	typeNames  []string
+	changeFunc func(*goast.FuncDecl)
+}
+
+var intrinsicFunction = map[string]ifunc{
+	"real": ifunc{
+		typeNames: []string{"float64"},
 		changeFunc: func(f *goast.FuncDecl) {
 			f.Name = goast.NewIdent("real")
 		},
 	},
-	{
-		functionName: "aimag",
-		typeNames:    []string{"float64"},
+	"aimag": ifunc{
+		typeNames: []string{"float64"},
 		changeFunc: func(f *goast.FuncDecl) {
 			f.Name = goast.NewIdent("imag")
 		},
