9c78a337cfe6ca257d3c3a63c8271d41802277bf
Konstantin8105 <konstantin8105@gmail.com>
2019-10-16 00:49:33 +0300

    add test for TESTING

--- main_test.go
+++ main_test.go
@@ -212,6 +212,25 @@ func TestBlas(t *testing.T) {
 				t.Logf("Error is not empty")
 				amount++
 			}
+
+			if !strings.Contains(ss[i], "TESTING") {
+				return
+			}
+
+			// Go name
+			goname := ss[i]
+			if index := strings.LastIndex(goname, "."); index > 0 {
+				goname = goname[:index] + ".go"
+			}
+
+			// run Go test
+			cmd := exec.Command(
+				"go", "build", goname,
+			)
+			goOutput, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Errorf("Cannot go executable file : %v\n%s", err, goOutput)
+			}
 		})
 	}
 
